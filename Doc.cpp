e3ccb1cf2b1275734b854eb01f45a20743f31c9b
Skyplasma <167046147+Skyplasma@users.noreply.github.com>
2025-02-20 12:58:02 +1100

    Add files via upload

---
+++ Doc.cpp
@@ -0,0 +1,49 @@
+
+
+
+// C++ code
+int ledOn =8; 
+int i= 7;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode (2,INPUT);
+  pinMode (3,INPUT);
+}
+void loop()
+{
+  Serial.print(i);
+  Serial.println(ledOn);
+  if (digitalRead(3)==HIGH){
+    for (ledOn;ledOn<=i;ledOn++){
+      digitalWrite(ledOn,HIGH);
+      delay(200);
+    }
+   i++;
+  }
+  if (digitalRead(2)==HIGH){
+    for (i;i<=ledOn;ledOn--){
+      digitalWrite(ledOn,LOW);
+      delay(200);
+    }
+    i--;
+  }
+  if(ledOn >= 14){
+    for (i;i>=8;i--){
+      digitalWrite(i,LOW);
+    }
+   ledOn = 8;
+  }
+  if(ledOn == 6){
+    for (i;i<=12;i++){
+      digitalWrite(i,HIGH); 
+    }
+    ledOn = 13;
+  }
+}
+
