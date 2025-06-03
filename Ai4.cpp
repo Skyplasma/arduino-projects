8f88f7c9be147344ed3b6c40cee9f782767db5a3
Skyplasma <167046147+Skyplasma@users.noreply.github.com>
2025-06-03 11:39:11 +1000

    Update Ai4.cpp

--- Ai4.cpp
+++ Ai4.cpp
@@ -1,35 +1,35 @@
-int enableA 13;
-int enableB 8;
-int in1 12;
-int in2 11;
-int in3 10;
-int in4 9;
+int enableA = 13;
+int enableB = 8;
+int in1 = 12;
+int in2 = 11;
+int in3 = 10;
+int in4 = 9;
 
 int speed;
 
-void setup(){
-    pinMode(in1,OUTPUT);
-    pinMode(in2,OUTPUT);
-    speed = 255;
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  speed = 255;
 }
-void loop(){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
-    for(int i = 0,i <= 123,i++){
-        analogWrite(enableA,(speed--i));
-        analogWrite(enableB,speed);
-        Wait(50);
-    }
-    for(int i = 0,i <= 123,i++){
-        analogWrite(enableA,speed);
-        analogWrite(enableB,speed--i);
-        Wait(100);
-    }
-    for(int = 123,i<=0,i--){
-        analogWrite(enableA,(speed--i));
-        analogWrite(enableB,speed);
-        Wait(50);
-    }
+void loop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  for (int i = 0; i <= 123; i++) {
+    analogWrite(enableA, speed - i);
+    analogWrite(enableB, speed);
+    delay(50);
+  }
+  for (int i = 0; i <= 123; i++) {
+    analogWrite(enableA, speed);
+    analogWrite(enableB, speed - i);
+    delay(100);
+  }
+  for (int i = 123; i <= 0; i--) {
+    analogWrite(enableA, speed - i);
+    analogWrite(enableB, speed);
+    delay(50);
+  }
 }
